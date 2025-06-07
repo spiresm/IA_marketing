15b3283dbaa60a9ce31653065b33ebb3cbb2a7e6
steeve pires madeira <119943572+spiresm@users.noreply.github.com>
2025-06-07 17:52:07 +0200

    Update getDemandesIA.js

--- netlify/functions/getDemandesIA.js
+++ netlify/functions/getDemandesIA.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const FILE_PATH = path.join(__dirname, '../../data/demandes.json');
+const FILE_PATH = path.join(__dirname, '../data/demandes.json');
 
 exports.handler = async () => {
   try {
