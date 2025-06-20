bc6ca998ceb16f9287099c0055440acb149d9fc0
steeve pires madeira <119943572+spiresm@users.noreply.github.com>
2025-06-20 02:45:32 +0200

    Update server.js

--- mon-chatbot/server.js
+++ mon-chatbot/server.js
@@ -22,7 +22,7 @@ try {
 }
 
 // 🌐 URL du webhook N8N
-const WEBHOOK_URL = 'https://9cd2-169-155-241-172.ngrok-free.app -> http://localhost:5678'; // 🔁 À remplacer par ta vraie URL N8N
+const WEBHOOK_URL = 'https://cf59-169-155-241-172.ngrok-free.app -> http://localhost:5678'; // 🔁 À remplacer par ta vraie URL N8N
 
 // 🧠 Endpoint principal avec appel au webhook N8N
 app.post('/api/chat', async (req, res) => {
